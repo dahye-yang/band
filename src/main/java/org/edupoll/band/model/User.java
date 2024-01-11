43a35fe118602acee7deb2a4c8e8b546b1263805
happyhyeri <hyeri2007@naver.com>
2024-01-11 09:01:35 +0900

    loinging
    소셜 네트워크 로그인 진행중

--- src/main/java/org/edupoll/band/model/User.java
+++ src/main/java/org/edupoll/band/model/User.java
@@ -1,10 +1,27 @@
 package org.edupoll.band.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Setter
 @Getter
-public class User {
+public class User {   
 	private String userId;
+	private String userPassword;
+	private String platform;
+	private String accessToken;
+	private String phoneNumber;
+	private Date birth;
+	private String gender; 
+	
+	private Profile profile;
 }
