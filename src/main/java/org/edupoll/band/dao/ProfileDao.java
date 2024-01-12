60a513f23928055c66f39265e2b25bf6bb938520
happyhyeri <hyeri2007@naver.com>
2024-01-12 15:57:14 +0900

    login/register
    로그인이랑 회원가입 처리

--- src/main/java/org/edupoll/band/dao/ProfileDao.java
+++ src/main/java/org/edupoll/band/dao/ProfileDao.java
@@ -1,12 +1,14 @@
 package org.edupoll.band.dao;
 
+import java.util.List;
+
 import org.edupoll.band.model.Profile;
 
 public interface ProfileDao {
 	
 	//프로필 
-		public Profile profileSave(Profile one);
+		public int profileSave(Profile one);
 		public int profileUpdate(Profile one);
-		public Profile findProfileById (String id);
+		public List<Profile> findProfileById (String id);
 
 }
